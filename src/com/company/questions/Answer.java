6189d629efd3deaac9e9680a7f8f588b87836862
P.S <pslatkin20@windwardschool.org>
2019-02-28 09:45:15 -0800

    Modify Answer

--- src/com/company/questions/Answer.java
+++ src/com/company/questions/Answer.java
@@ -2,10 +2,47 @@ package com.company.questions;
 
 import com.company.TreeNode;
 
-public class Answer extends TreeNode<String>
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Answer extends Question
 {
-    public Answer(String s) {
-        System.out.println(s);
-   //     System.exit(0);
+    private String s;
+    public Answer(String s, boolean fin)
+    {
+        super("Is it " + s + "?");
+        this.s = s;
+        if(fin)
+        {
+            System.out.println(s);
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public TreeNode<String> left()
+    {
+        return new Answer("It is " + s , true);
+    }
+    @Override
+    public TreeNode<String> right()
+    {
+        try
+        {
+            System.out.println("Damn, who is it?");
+            Scanner s = new Scanner(System.in);
+            String person = s.nextLine();
+            File f = new File("nodestoadd.txt");
+            if(!f.exists()) f.createNewFile();
+            Files.write(f.toPath(), Arrays.asList(person), StandardOpenOption.APPEND);
+            System.exit(0);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
